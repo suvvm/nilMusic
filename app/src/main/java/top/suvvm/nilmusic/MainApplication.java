bb4817b1429d113f3f564c7ea16bf25035dd9e62
suvvm <915269675@qq.com>
2020-04-28 20:36:38 +0800

    creact & regist MainApplication

---
+++ app/src/main/java/top/suvvm/nilmusic/MainApplication.java
@@ -0,0 +1,10 @@
+package top.suvvm.nilmusic;
+
+import android.app.Application;
+
+public class MainApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
