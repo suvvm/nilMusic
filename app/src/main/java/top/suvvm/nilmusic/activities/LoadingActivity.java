96263a44cd7c1ea2a27a2633085d7692825724da
suvvm <915269675@qq.com>
2020-04-28 22:46:52 +0800

    create LoadingActivity and layout

---
+++ app/src/main/java/top/suvvm/nilmusic/activities/LoadingActivity.java
@@ -0,0 +1,12 @@
+package top.suvvm.nilmusic.activities;
+
+import android.os.Bundle;
+import top.suvvm.nilmusic.R;
+
+public class LoadingActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+    }
+}
