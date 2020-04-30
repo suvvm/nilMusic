813ddca1b5c2401e2e8bd829b46308c57200888c
suvvm <915269675@qq.com>
2020-04-30 22:10:49 +0800

    LoginActivity extends BaseActivity

---
+++ app/src/main/java/top/suvvm/nilmusic/activities/LoginActivity.java
@@ -0,0 +1,20 @@
+package top.suvvm.nilmusic.activities;
+
+import top.suvvm.nilmusic.R;
+
+import android.os.Bundle;
+
+/**
+ * @ClassName: LoadingActivity
+ * @Description: 登录活动
+ * @Author: SUVVM
+ * @Date: 2020/04/30 21:28
+ */
+public class LoginActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
